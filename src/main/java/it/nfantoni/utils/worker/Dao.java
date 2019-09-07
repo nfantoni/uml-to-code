0e9f81eed8afaec6160efb928facab470b778e0a
e8035552 <nfantoni@imolainformatica.it>
2019-09-07 11:45:27 +0200

    add dao impl

--- src/main/java/it/nfantoni/utils/worker/Dao.java
+++ src/main/java/it/nfantoni/utils/worker/Dao.java
@@ -1,9 +1,24 @@
 package it.nfantoni.utils.worker;
 
+import it.nfantoni.utils.entities.Entity;
+import it.nfantoni.utils.settings.Settings;
+
+import java.io.File;
+import java.util.List;
+
 public class Dao implements Worker {
 
     @Override
-    public void work() {
+    public void work(Settings settings, List<Entity> entityList) {
         //TODO implements for Dao Pattern
+
+        File directory = new File(settings.getOutputPath());
+        directory.mkdirs();
     }
+
+    private void writeDaoEntity(Entity entity){
+
+
+    }
+
 }
